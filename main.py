bc6222b8fae24f962956fa62308980d6c77b89bb
LasRom <lastuvkaroman38@gmail.com>
2022-08-03 11:03:33 +0300

    fd

--- main.py
+++ main.py
@@ -2,22 +2,13 @@ import vk_api
 import datetime
 import time
 
-schet = 1000
 vk = vk_api.VkApi(
-        token="")
-last_dt = datetime.datetime.now()
+    token="your_token")
+last_dt = datetime.datetime.now()  # получаю текущую дату и время
 while True:
-    try:
-        if str(last_dt).split()[1].split(':')[1] != str(datetime.datetime.now()).split()[1].split(':')[1]:
-            print(1)
-            dt = datetime.datetime.now()
-            vk.method("status.set", {"text": f'{dt.strftime("Date: %d-%m-%Y  time: %H:%M")}'})
-            last_dt = datetime.datetime.now()
-            time.sleep(60)
-        else:
-            print(2)
-            continue
-    except BaseException as e:
-        print(e)
-        vk = vk_api.VkApi(
-            token="")
+    # проверяю, прошла ли минута, чтобы обновить статус
+    if str(last_dt).split()[1].split(':')[1] != str(datetime.datetime.now()).split()[1].split(':')[1]:
+        dt = datetime.datetime.now()  # получаю текущую дату и время
+        vk.method("status.set", {"text": f'{dt.strftime("Date: %d-%m-%Y  time: %H:%M")}'})  # обновляю статус
+        last_dt = dt  # меняю последнее время
+        time.sleep(60)  # заставляем код "заснуть", чтобы обойти капчу вк
