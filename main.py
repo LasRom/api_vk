c58089912e66e67d1502e01d47fc0a62e4cd659b
LasRom <lastuvkaroman38@gmail.com>
2022-08-03 10:59:31 +0300

    fd

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import vk_api
+import datetime
 import time
 
 schet = 1000
+vk = vk_api.VkApi(
+        token="")
+last_dt = datetime.datetime.now()
 while True:
-    vk = vk_api.VkApi(
-        token="vk1.a.SCUc7l99Tw9EZUUbsot_rMkXAX3WnUAJvdgjoYQB78CcB-L8sFTGSnbDycjdjEemMmYX6h09deQdwvNDVtYjzywHr39U6FH5tVvI1cdtg41l48-KAE_NY5Sjf5nhCYPrOafBM-4fcJbHbM6nfqAS5TcaQVDLBYUGkByzzdw2MPp6v8Ftq-HsIPh8LaBNMHQM")
-    vk.method("status.set", {"text": f"1000 - 7 (online every 30 seconds) {schet}"})
-    schet -= 7
-    if schet < 0:
-        schet = 1000
-    time.sleep(30)  # погружаем скрипт в «сон» на 30 секунд
+    try:
+        if str(last_dt).split()[1].split(':')[1] != str(datetime.datetime.now()).split()[1].split(':')[1]:
+            print(1)
+            dt = datetime.datetime.now()
+            vk.method("status.set", {"text": f'{dt.strftime("Date: %d-%m-%Y  time: %H:%M")}'})
+            last_dt = datetime.datetime.now()
+            time.sleep(60)
+        else:
+            print(2)
+            continue
+    except BaseException as e:
+        print(e)
+        vk = vk_api.VkApi(
+            token="")
